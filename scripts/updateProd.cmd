0de10b1042a8bdcbc82fb9d422ce6e0ab348b082
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-09-23 13:19:04 -0300

    chore: update branch main on updateProd script

--- scripts/updateProd.cmd
+++ scripts/updateProd.cmd
@@ -4,6 +4,16 @@ IF NOT %ERRORLEVEL% == 0 (
     exit /b %ERRORLEVEL%
 )
 
+git fetch
+
+git pull
+
+git checkout main
+
+git merge origin/develop
+
+git checkout develop
+
 ssh root@15.229.18.114 "pm2 delete ssr.finax"
 
 ssh root@15.229.18.114 "rm -rf /var/www/html/finax/*"
--- scripts/updateProd.sh
+++ scripts/updateProd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Executa o build em modo produção
 npm run build:prod
 
 # Verifica se o build foi bem-sucedido
@@ -9,14 +8,20 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Deleta o processo existente do PM2 no servidor remoto
+git fetch
+
+git pull
+
+git checkout main
+
+git merge origin/develop
+
+git checkout develop
+
 ssh root@15.229.18.114 "pm2 delete ssr.finax"
 
-# Remove os arquivos antigos no servidor remoto
 ssh root@15.229.18.114 "rm -rf /var/www/html/finax/*"
 
-# Copia os novos arquivos para o servidor remoto
 scp -r dist/finax-front/* root@15.229.18.114:/var/www/html/finax/
 
-# Inicia o processo no servidor remoto
 ssh root@15.229.18.114 "/root/start_finax.sh"
