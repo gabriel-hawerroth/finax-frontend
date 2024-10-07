94d5b1c59295b2a43ce612f3d1bc684208c31d15
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-10-07 18:23:20 -0300

    refact: improving app-buttons component (it's broken)

--- src/app/shared/components/buttons/styled-button/styled-button.component.ts
+++ src/app/shared/components/buttons/styled-button/styled-button.component.ts
@@ -8,8 +8,8 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ButtonType } from '../../../../core/enums/button-style';
-import { getBtnStyle } from '../../../utils/utils';
 import { BtnContentComponent } from './btn-content/btn-content.component';
+import { getBtnStyle } from '../../../utils/utils';
 
 @Component({
   selector: 'styled-button',
@@ -25,17 +25,38 @@ import { BtnContentComponent } from './btn-content/btn-content.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StyledButtonComponent {
-  getBtnStyle = getBtnStyle;
-
   onClick = output<void>();
-  btnType = input.required<ButtonType>();
 
+  btnType = input.required<ButtonType>();
   label = input.required<string>();
   icon = input.required<string>();
   disabled = input<boolean>();
   loading = input<boolean>();
   btnStyle = input<{}>();
   contentStyle = input<{}>();
+
+  getMatButtonClass(): string {
+    switch (this.btnType()) {
+      case getBtnStyle('raised'):
+        return 'mat-raised-button';
+      case getBtnStyle('stroked'):
+        return 'mat-stroked-button';
+      case getBtnStyle('flat'):
+        return 'mat-flat-button';
+      case getBtnStyle('icon'):
+        return 'mat-icon-button';
+      case getBtnStyle('fab'):
+      case getBtnStyle('extended-fab'):
+      case getBtnStyle('mini-fab'):
+        return 'mat-fab';
+      default:
+        return 'mat-button';
+    }
+  }
+
+  isIconButton(): boolean {
+    return this.btnType() === getBtnStyle('icon');
+  }
 
   public get getIconBtnStyle() {
     return {
