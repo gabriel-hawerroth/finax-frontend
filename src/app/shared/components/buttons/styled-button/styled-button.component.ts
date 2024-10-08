b4cb5e525b698383dd4a1c6d742f1a90f26aaa30
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-10-08 07:46:47 -0300

    Revert "refact: improving app-buttons component (it's broken)"
    This reverts commit 94d5b1c59295b2a43ce612f3d1bc684208c31d15.

--- src/app/shared/components/buttons/styled-button/styled-button.component.ts
+++ src/app/shared/components/buttons/styled-button/styled-button.component.ts
@@ -8,8 +8,8 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ButtonType } from '../../../../core/enums/button-style';
-import { BtnContentComponent } from './btn-content/btn-content.component';
 import { getBtnStyle } from '../../../utils/utils';
+import { BtnContentComponent } from './btn-content/btn-content.component';
 
 @Component({
   selector: 'styled-button',
@@ -25,38 +25,17 @@ import { getBtnStyle } from '../../../utils/utils';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StyledButtonComponent {
-  onClick = output<void>();
+  getBtnStyle = getBtnStyle;
 
+  onClick = output<void>();
   btnType = input.required<ButtonType>();
+
   label = input.required<string>();
   icon = input.required<string>();
   disabled = input<boolean>();
   loading = input<boolean>();
   btnStyle = input<{}>();
   contentStyle = input<{}>();
-
-  getMatButtonClass(): string {
-    switch (this.btnType()) {
-      case getBtnStyle('raised'):
-        return 'mat-raised-button';
-      case getBtnStyle('stroked'):
-        return 'mat-stroked-button';
-      case getBtnStyle('flat'):
-        return 'mat-flat-button';
-      case getBtnStyle('icon'):
-        return 'mat-icon-button';
-      case getBtnStyle('fab'):
-      case getBtnStyle('extended-fab'):
-      case getBtnStyle('mini-fab'):
-        return 'mat-fab';
-      default:
-        return 'mat-button';
-    }
-  }
-
-  isIconButton(): boolean {
-    return this.btnType() === getBtnStyle('icon');
-  }
 
   public get getIconBtnStyle() {
     return {
