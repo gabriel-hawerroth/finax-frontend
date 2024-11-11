3a768422203292d1d8fcc38906489c3346a39ee9
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-11-10 23:58:30 -0300

    feat: change cloud front url on dev environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   mode: 'development',
   production: false,
   baseApiUrl: 'http://localhost:8080/',
-  cloudFrontUrl: 'https://d3dpbaewouik5z.cloudfront.net',
+  cloudFrontUrl: 'https://d2u9e09dzodebk.cloudfront.net',
 };
