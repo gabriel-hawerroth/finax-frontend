f10209bae96404b7d0e121f8a1c8cac483f40f54
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-11-26 07:49:54 -0300

    feat: remove footer on app-page

--- src/app/main/pages/mobile-page/views/principal/mobile-page.component.ts
+++ src/app/main/pages/mobile-page/views/principal/mobile-page.component.ts
@@ -1,13 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { PublicFooterComponent } from '../../../../public/components/footer/public-footer.component';
+import { cloudFireCdnImgsLink } from '../../../../../shared/utils/utils';
 import { PublicHeaderComponent } from '../../../../public/components/header/public-header.component';
 
 @Component({
   selector: 'app-mobile-page',
   standalone: true,
-  imports: [PublicHeaderComponent, PublicFooterComponent],
+  imports: [PublicHeaderComponent],
   templateUrl: './mobile-page.component.html',
   styleUrl: './mobile-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MobilePage {}
+export class MobilePage {
+  cloudFireCdnImgsLink = cloudFireCdnImgsLink;
+}
