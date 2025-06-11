6ee8ecd12ecc0815b7fbe1484ed8d07a5c9563e3
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2025-06-11 16:25:45 -0300

    feat: add session replay configuration to Sentry integration

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,10 @@ Sentry.init({
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
+  integrations: [Sentry.replayIntegration()],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
