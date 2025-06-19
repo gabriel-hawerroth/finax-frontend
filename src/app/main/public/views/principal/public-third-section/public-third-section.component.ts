1f60ee55e1960b843da37526b5039194d12296fd
gabriel-hawerroth <gabrielhawerroth04@gmail.com>
2025-06-19 20:13:38 -0300

    feat: update public sections with improved styling and layout, enhance third section with dynamic card data

--- src/app/main/public/views/principal/public-third-section/public-third-section.component.ts
+++ src/app/main/public/views/principal/public-third-section/public-third-section.component.ts
@@ -1,16 +1,45 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSlideToggle } from '@angular/material/slide-toggle';
-import { RouterModule } from '@angular/router';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-public-third-section',
-  imports: [ReactiveFormsModule, MatSlideToggle, MatButtonModule, RouterModule],
+  imports: [MatListModule],
   templateUrl: './public-third-section.component.html',
   styleUrl: './public-third-section.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PublicThirdSectionComponent {
-  switchPlansControl: FormControl = new FormControl(false);
+  cards = signal<ThirdSectionCard[]>([
+    {
+      icon: 'credit_card',
+      iconBackgroundColor: '#009cbf',
+      title: 'Transações',
+      description: 'Controle completo',
+    },
+    {
+      icon: 'savings',
+      iconBackgroundColor: '#00bf2d',
+      title: 'Economia',
+      description: 'Metas inteligentes',
+    },
+    {
+      icon: 'bar_chart',
+      iconBackgroundColor: '#a600bf',
+      title: 'Análises',
+      description: 'Insights com IA',
+    },
+    {
+      icon: 'trending_up',
+      iconBackgroundColor: '#e47f04',
+      title: 'Investimentos',
+      description: 'Acompanhamento',
+    },
+  ]);
+}
+
+interface ThirdSectionCard {
+  icon: string;
+  iconBackgroundColor: string;
+  title: string;
+  description: string;
 }
