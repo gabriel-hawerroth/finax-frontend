2999afab9e2297738dcb6836247c092de7f740c0
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2026-02-22 12:43:11 -0300

    feat: enhance notPwaGuard to navigate to login for non-standalone users

--- src/app/core/guards/not-pwa.guard.ts
+++ src/app/core/guards/not-pwa.guard.ts
@@ -1,17 +1,20 @@
 import { isPlatformServer } from '@angular/common';
 import { inject, PLATFORM_ID } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const notPwaGuard: CanActivateFn = (
   route,
   state,
   platformId = inject(PLATFORM_ID),
+  router = inject(Router),
 ) => {
   if (isPlatformServer(platformId)) return false;
 
   const isStandalone =
     window.matchMedia('(display-mode: standalone)').matches ||
     (navigator as any).standalone === true;
+
+  if (!isStandalone) router.navigateByUrl('login');
 
   return !isStandalone;
 };
